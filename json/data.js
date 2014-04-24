896f306ebc4aa75e6424870bba4057b8df1656ae
qwerytiop <qwerytiop@gmail.com>
2014-04-23 17:36:07 -0700

    more testing for msjcs

---
+++ json/data.js
@@ -0,0 +1,22 @@
+function setup(){
+data = new Object();
+//posts.json retrieve
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+ 
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    data.posts=JSON.parse(xmlhttp.responseText);
+    }
+  }
+
+xmlhttp.open("GET","posts.json",true);
+xmlhttp.send();
+
+return loader();
+}
